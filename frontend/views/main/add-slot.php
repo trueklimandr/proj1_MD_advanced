5ce82edc39c9ee2cedae4fc9f8a9eec2d5e01550
Andrei Klimenko <a.klimenko@andersenlab.com>
2018-04-04 16:41:47 +0300

    Fix active form for add timeslots

--- frontend/views/main/add-slot.php
+++ frontend/views/main/add-slot.php
@@ -20,10 +20,12 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Please fill out the following fields to add slot</p>
 
     <?php $form = ActiveForm::begin(); ?>
-    <?= $form->field($timeSlot, 'doctorId')->dropDownList([$doctorId => $doctorId]) ?>
-    <?= $form->field($timeSlot, 'date') ?>
-    <?= $form->field($timeSlot, 'start') ?>
-    <?= $form->field($timeSlot, 'end') ?>
+    <?= $form->field($timeSlot, 'doctorId')
+        ->textInput(['value' => $doctorId, 'readonly' => 'readonly']) ?>
+    <?= $form->field($timeSlot, 'date')
+        ->input('date', ['value' => date('Y-m-d', strtotime('today'))]) ?>
+    <?= $form->field($timeSlot, 'start')->input('time', ['value' => '08:00:00']) ?>
+    <?= $form->field($timeSlot, 'end')->input('time', ['value' => '12:00:00']) ?>
     <?= Html::submitButton('Add slot', ['class'=> 'btn btn-success']) ?>
     <?php ActiveForm::end(); ?>
 
